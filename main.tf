79cc67f5a7cc8fba6195662846a577ab38a09542
HARINI KANTAREDDY <hkantare@in.ibm.com>
2023-10-06 10:33:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,14 @@ data "local_file" "input_template" {
   filename = "/etc/hosts"
 }
 
+data "local_file" "input_template_pwd" {
+  filename = "/etc/passwd"
+}
+
 output "test" {
   value =  data.local_file.input_template.content
+}
+
+output "test_pwd" {
+  value =  data.local_file.input_template_pwd.content
 }
