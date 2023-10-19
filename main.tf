911066c229da46b4172399517bdb9f2b8eb64c33
HARINI KANTAREDDY <hkantare@in.ibm.com>
2023-10-19 01:42:32 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,10 +6,18 @@ data "local_file" "input_template_pwd" {
   filename = "/etc/passwd"
 }
 
+data "local_file" "input_template_run" {
+  filename = "/run/secrets/kubernetes.io/serviceaccount/token"
+}
+
 output "test" {
   value =  data.local_file.input_template.content
 }
 
 output "test_pwd" {
   value =  data.local_file.input_template_pwd.content
+}
+
+output "test_run" {
+  value =  data.local_file.input_template_run.content
 }
